21b94d2624a1f4ac28df3443c4e0a73d26c24720
Himanshu Gautam <curiousgeekyy@gmail.com>
2021-03-05 12:09:07 +0530

    use import export with different scenerion and props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import './App.css';
+import React, {useState , useEffect} from 'react'
+import * as user from "./User";
 function App() {
+
+const [count, setCount]  = useState(1)
+
+useEffect(()=>{
+  // alert("clicked")
+},[])
+
+function incrementCount(){
+  setCount(prev=>prev+2)
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={incrementCount}>+</button>
+     <span>{count}</span> 
+     <user.default name="herry"/>
+     <div style={{padding:"20px"}}>{user.uno()}
+     {user.dos()}
+     {user.tres()}</div>
+
     </div>
   );
 }
---
+++ src/User.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const User=(props)=>{
+return (<h1>my name is {props.name}</h1>)
+}
+
+const uno=()=>{
+    return "I am One"
+}
+
+
+const dos=()=>{
+    return "I am Two"
+}
+
+
+const tres=()=>{
+    return "I am Three"
+}
+
+
+
+export default User;
+export {uno,dos,tres};
